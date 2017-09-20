3d0c829644726e62c2dbb7afc381f543b3396636
David Simonson <David.Simonson@chrobinson.com>
2017-09-20 08:32:35 -0500

    added

---
+++ src/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+console.log("Thank you, " + firstName + " " + lastName);
+console.log("I see you are " + age + " years old.");
